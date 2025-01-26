f029be6bb616d18ea250b03f1a5e3abd23258d81
scar17off <81940406+scar17off@users.noreply.github.com>
2025-01-26 12:53:30 +0200

    Updated the route directory

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 
 // Serve static files from the React build directory
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
